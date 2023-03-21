f6ab9686d9b63f38f6fa445b9a363d486b07e0f8
Kevin Liu <liukproductivity@gmail.com>
2023-03-21 13:56:20 -0400

    Exercise 2 complete

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,33 +3,24 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
+import GuessResults from "../GuessResults/GuessResults";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-function handleSubmitGuess(event) {
-  event.preventDefault();
-  const isCorrect = event === answer;
-}
-
 function Game() {
-  const [words, setWords] = React.useState([]);
+  const [guesses, setGuesses] = React.useState([]);
 
-  // function addGuess(guessWord) {
-  //   const newGuess = {
-  //     guessWord,
-  //     id: Math.random(),
-  //   };
-
-  //   const allGuesses = [...words, newGuess];
-  //   setWords(allGuesses);
-  // }
+  function handleSubmitGuess(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess]);
+  }
 
   return (
     <div>
-      <GuessInput />
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </div>
   );
 }
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,24 +1,26 @@
 import React from "react";
 
-function GuessInput({}) {
-  const [guess, setGuess] = React.useState("");
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess("");
+  }
   return (
-    <form
-      className="guess-input-wrapper"
-      onSubmit={(event) => {
-        event.preventDefault();
-        console.log({ guess });
-        setGuess("");
-      }}
-    >
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required
         id="guess-input"
         type="text"
-        value={guess}
+        value={tentativeGuess}
         pattern="[A-Z]{5}"
-        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+        title="5 letter word"
+        onChange={(event) =>
+          setTentativeGuess(event.target.value.toUpperCase())
+        }
       />
     </form>
   );
